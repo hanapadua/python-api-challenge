5e01c271ffbb4a14a065796bf45492591bde1c18
hanapadua <hana.padua101@gmail.com>
2021-08-01 14:11:34 -0400

    hw push

---
+++ VacationPy/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "50d6490911019c6cacc735710f16ebcc"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
---
+++ VacationPy/config.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "50d6490911019c6cacc735710f16ebcc"
+
+# Google API Key
+g_key = "AIzaSyDWiwRvNdWlHcTMe9xJs4p9jVCdsNU1pQ8"
